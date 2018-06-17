98077a5d7478cf1ac6cc383ea91ae5981d922e25
pf4edward <pf4edward@openmailbox.org>
2018-06-16 22:18:50 -0400

    the xml and java was not working from the githubbed version.
    So, it was created as a new project, code added, debugged and tested, then
    fitted into the files from github.  Now submitted.

--- app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
+++ app/src/main/java/com/londonappbrewery/magiceightball/MainActivity.java
@@ -2,6 +2,12 @@ package com.londonappbrewery.magiceightball;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.Random;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +15,29 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final ImageView ballDisplay = findViewById(R.id.image_ballDisplay);
+
+        Button myButton = findViewById(R.id.askButton);
+
+        final int[] ballArray = new int[]{
+                R.drawable.ball1,
+                R.drawable.ball2,
+                R.drawable.ball3,
+                R.drawable.ball4,
+                R.drawable.ball5
+        };
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Random randomNumberGenerator = new Random();
+                int number = randomNumberGenerator.nextInt(5);
+
+                int imageResourceId = ballArray[number];
+                ballDisplay.setImageResource(imageResourceId);
+
+            }
+        });
     }
 }
